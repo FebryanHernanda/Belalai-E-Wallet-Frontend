f1d0afcaad6412d1a115edabfeb7756741bb72a5
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-24 13:55:23 +0700

    feat(main): initial starter boilerplate

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,20 @@
+// import "./index.css";
+
+function MainRoutes() {
+  return (
+    <>
+      <nav>
+        <h1 className="text-red-500">Home</h1>
+      </nav>
+
+      {/* Routes */}
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes> */}
+      {/* Routes */}
+    </>
+  );
+}
+
+export default MainRoutes;
