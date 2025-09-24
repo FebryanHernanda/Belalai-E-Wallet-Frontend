f09a4e51673315e8a2d1faca4cc61c0a6394ef5f
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-24 23:32:38 +0700

    feat(layouts/main): add sidebar to mainlayouts

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/navbar";
+import Sidebar from "../components/sidebar/Sidebar";
 
 const MainLayout = () => {
   return (
     <>
       <Navbar />
-      <main>
-        <Outlet />
+      <main className="min-h-screen flex">
+        <Sidebar />
+        <div className="w-full">
+          <Outlet />
+        </div>
       </main>
       <Footer />
     </>
