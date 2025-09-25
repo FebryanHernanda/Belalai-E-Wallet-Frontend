3500b2a69ee0d8c76fa2515c2c6392dfe5604dcd
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-25 13:55:56 +0700

    feat(layout/main): move navbar state to parent for handling open and close menu

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router-dom";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/navbar/navbar";
 import Sidebar from "../components/sidebar/Sidebar";
+import { useState } from "react";
 
 const MainLayout = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <>
-      <Navbar />
-      <main className="min-h-screen flex">
+      <Navbar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      <main className="min-h-screen flex" onClick={() => setIsMenuOpen(false)}>
         <Sidebar />
         <div className="w-full relative">
           <Outlet />
