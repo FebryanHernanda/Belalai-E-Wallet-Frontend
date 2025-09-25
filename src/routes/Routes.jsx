28ca323efa9911d34961a040b98dd0da1f04840c
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-25 14:22:19 +0700

    feat(routes): add main routes integrations

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,16 +2,21 @@ import { Route, Routes } from "react-router-dom";
 import Register from "../pages/register/Register";
 import Login from "../pages/login/login";
 import EnterPin from "../pages/enter-pin/EnterPin";
-import MainLayout from "../layouts/MainLayout";
+import LandingLayout from "../layouts/LandingLayout";
 
 function MainRoutes() {
   return (
     <>
       {/* Routes */}
       <Routes>
-        <Route path="/landing" element={<MainLayout />}></Route>
+        {/* Landing Routes */}
+        <Route path="/" element={<LandingLayout />}>
+          {/* Content */}
+        </Route>
+        {/* Landing Routes */}
+
         {/* Auth Routes */}
-        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/login/enter-pin" element={<EnterPin />} />
         <Route path="/register" element={<Register />} />
         {/* Auth Routes */}
