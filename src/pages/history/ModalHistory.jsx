1956507003b639f51df3c46feb975e947a10ecbd
FebryanHernanda <febryanhernanda@gmail.com>
2025-09-28 22:37:13 +0700

    feat(ModalHistory): add delete function on modal mobile

--- src/pages/history/ModalHistory.jsx
+++ src/pages/history/ModalHistory.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 import { API_URL } from "../../utils";
 import ModalDelete from "../modal/ModalDelete";
+import { toast } from "react-toastify";
+import {
+  deleteHistory,
+  deleteTopupHistory,
+  getHistory,
+} from "../../store/transferSlice";
+import { useDispatch } from "react-redux";
+import getPaymentMethods from "../../utils/getPaymentMethods";
 
 function ModalHistory(props) {
+  const dispatch = useDispatch();
   const { setIsOpen, customer } = props;
   const [Active, SetActive] = useState(false);
+
+  const handleDelete = async (historyData) => {
+    try {
+      if (historyData.transaction_type === "Topup") {
+        dispatch(deleteTopupHistory(historyData.id));
+      } else {
+        dispatch(deleteHistory(historyData.id));
+      }
+
+      dispatch(getHistory());
+      toast.success("Berhasil Menghapus histori", { autoClose: 1500 });
+      setIsOpen((prev) => !prev);
+    } catch (error) {
+      console.log(error);
+      toast.error("Gagal Menghapus histori", { autoClose: 1500 });
+    }
+  };
+
   return (
     <>
       {Active && <ModalDelete onClose={() => SetActive(false)} />}
@@ -12,11 +39,19 @@ function ModalHistory(props) {
         <h1 className="text-gray-500">DETAIL TRANSACTION</h1>
         <hr className="border-t border-gray-500" />
         <div className="py-5">
-          <img
-            src={`${API_URL}/img/${customer?.profile_picture}`}
-            alt=""
-            className="max-w-20"
-          />
+          {customer.transaction_type === "Topup" ? (
+            <img
+              src={getPaymentMethods(customer?.contact_name)}
+              alt="Payment Picture"
+              className="w-15 h-15  rounded-xl"
+            />
+          ) : (
+            <img
+              src={`${API_URL}/img/${customer?.profile_picture}`}
+              alt="Photo profile"
+              className="w-15 h-15 object-cover rounded-xl"
+            />
+          )}
         </div>
       </header>
       {/* pembungkus data user */}
@@ -26,8 +61,12 @@ function ModalHistory(props) {
           <p>{customer.contact_name}</p>
         </div>
         <div>
-          <h1 className="font-medium">Phone:</h1>
-          <p>{customer.phone_number}</p>
+          {customer.phone_number && (
+            <>
+              <h1 className="font-medium">Phone:</h1>
+              <p>{customer.phone_number} </p>
+            </>
+          )}
         </div>
         <div>
           <h1 className="font-medium">Status:</h1>
@@ -49,7 +88,7 @@ function ModalHistory(props) {
       <div className="px-5 mt-5 flex flex-col gap-5">
         <button
           className="border border-red-600 rounded-lg w-full flex justify-center items-center gap-5 h-10 cursor-pointer"
-          onClick={() => SetActive((prev) => !prev)}
+          onClick={() => handleDelete(customer)}
         >
           <img src="../src/assets/icon/delete.svg" alt="" />
           <p className="text-red-600 font-medium">Delete</p>
